6475c6a4d8690cbe9ae72517cc7f1571ffffe95e
Dylson <Dylsonuni@gmail.com>
2021-06-20 19:49:13 +0100

    completed the select criteria function

--- script.js
+++ script.js
@@ -18,15 +18,64 @@ THEN the password is either displayed in an alert or written to the page
 
 /* Declaring my global scope variables */
 var userChoice = "";
+var combineArr = "";
 /* Arrays used to hold in password elements. */
 var capitalArr = ["A", "B", "C", "D", "E"];
 var lowerArr = ["a", "b", "c", "d", "e"];
 var specialArr = ["!", "@", "#", "$", "%"];
 var numArr = [1, 2, 3, 4, 5];
 
+/* function generatePassword(){}; */
+
 /* function responsible for taking userinput and using it to select criteria for password */
 function selectCriteria() {
+  console.log(
+    "selectCriteria function has activated, userChoice is: " + userChoice
+  );
 
+  var findCapital = "CAPITAL";
+  var findNumber = "NUMBER";
+  var findSpecial = "SPECIAL";
+  /* console.log(userChoice.includes(findCapital)); */
+
+  /* Comment: I tried to incorporate code to account for more than one selected parameter.
+    if(userChoice.includes(findCapital) && userChoice.includes(findNumber)){
+      combineArr = capitalArr.concat(numArr).concat(lowerArr);
+      console.log("The word CAPITAL and NUMBER was found in the userChoice variable")
+      console.log("The combined Array is: " + combineArr)
+    }else if(userChoice.includes(findCapital)) {
+      console.log("The word CAPITAL was found in the userChoice variable.");
+      combineArr = capitalArr.concat(lowerArr);
+      console.log("The combined array is: "+ combineArr);
+    }else{
+      console.log("userChoice did not include CAPITAL");
+    } */
+
+  if (userChoice.includes(findCapital)) {
+    console.log("The word CAPITAL was found in the userChoice variable.");
+    var combineArr = capitalArr.concat(lowerArr);
+    console.log("The combined array is: " + combineArr);
+  } else {
+    console.log("userChoice did not include Capital.");
+  }
+
+  if (userChoice.includes(findNumber)) {
+    console.log("The word NUMBER was found in userChoice variable.");
+    var combineArr = numArr.concat(lowerArr);
+    console.log("The combined array is: " + combineArr);
+  } else {
+    console.log("userChoice did not include Number.");
+  }
+
+  if (userChoice.includes(findSpecial)) {
+    console.log("The word SPECIAL was found in userChoice variable.");
+    var combineArr = specialArr.concat(lowerArr);
+    console.log("The combined array is: " + combineArr);
+  } else {
+    console.log("userChoice did not include Special.");
+  }
+
+  console.log("End of select Criteria function.");
 }
 
 /* function responsible for the initial alerts and prompts. */
@@ -41,11 +90,17 @@ function initialPrompts() {
   /* Converts users input to upper case to minimize input mistakes */
   userChoice = userChoice.toUpperCase();
   /* Prints out what the user has selected in the console for developer to check their choice! Helps to see if code is operational. */
-  console.log("The user has selected " + userChoice + " as their criteria.");
+  console.log(
+    "The user has selected " +
+      userChoice +
+      " as their criteria. End of initialPrompts function"
+  );
 
   if (!userChoice) {
     return;
   }
+
+  selectCriteria();
 }
 
 document.getElementById("generate").addEventListener("click", initialPrompts);

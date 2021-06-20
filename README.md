c0996d0ba380368ef9e31bdeefc371c3332b1449
Dylson <Dylsonuni@gmail.com>
2021-06-20 16:28:24 +0100

    commenting out the switch statements & made arays.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Qwerty-Generator-HW3
-I have been tasked to modify starter-code to enable an application to generate random passwords based on a specific criteria set by the user. 
+I have been tasked to modify starter-code to enable an application to generate random passwords based on a specific criteria set by the user. It will feature dynamic HTML and CSS powered by JavaScript and will have a responsive UI that adjusts to multiple screen sizes.
--- script.js
+++ script.js
@@ -1,5 +1,84 @@
-// Assignment Code
+/* ACCEPTANCE CRITERIA
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+ */
+
+var capitalArr = ["A", "B", "C", "D", "E"];
+var lowerArr = ["a", "b", "c", "d", "e"];
+var specialArr = ["!","@","#","$","%"];
+var numArr = [1,2,3,4,5];
+
+
+function firstPrompt() {
+  /* I am asking the user to select criteria for their password */
+  var userChoice = window.alert(
+    "Before generating your password, you must select some criteria for your password. Click ok to follow on to the next set of instructions."
+  );
+  userChoice = window.alert(
+    "In the upcoming pop-up, type what set of criteria you may want for your password. The options you may select are: 'capital: for capital letters; special: for special characters; number: for numbers in your password. Click ok to proceed to the typing prompt.'"
+  );
+  userChoice = window.prompt("Type your criteria", "Capital; Number; Special");
+  /* Converts users input to upper case to minimize input mistakes */
+  userChoice = userChoice.toUpperCase();
+  /* Prints out what the user has selected in the console for developer to check their choice! Helps to see if code is operational. */
+  console.log("The user has selected " + userChoice + " as their criteria.");
+
+
+
+
+ /*  Decided to use a switch and case, to avoid the over use of if and else statements. 
+  switch (userChoice) {
+    case "CAPITAL":
+      console.log("the CAPITAL switch case is working fine!");
+      break;
+    case "SPECIAL":
+      console.log("the SPECIAL switch case is working fine!");
+      break;
+    case "NUMBER":
+      console.log("the NUMBER switch case is working fine!");
+      break;
+    case "CAPITAL" && "SPECIAL":
+      console.log("Both the CAPITAL and SPECIAL together case works fine.");  This is not working correctly will head back to this later.
+      break;
+    default:
+      console.log("Your input is invalid! Try again!");
+  }*  */
+
+
+
+
+  
+  if (!userChoice) {
+    return;
+  }
+}
+
+document.getElementById("generate").addEventListener("click", firstPrompt);
+
+/* // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+
+
+
+ function generatePassword(){
+  var userInput = 
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +86,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+ */
